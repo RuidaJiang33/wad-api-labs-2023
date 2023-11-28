5a6d05bbd5d46b6fa253a6b25fd72dfbe9e8d95d
RuidaJiang33 <772456648@qq.com>
2023-11-28 14:16:11 +0000

    Added JWT

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,7 +1,30 @@
 import express from 'express';
 import User from './userModel';
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); // eslint-disable-line
+
+async function registerUser(req, res) {
+  // Add input validation logic here
+  await User.create(req.body);
+  res.status(201).json({ success: true, msg: 'User successfully created.' });
+}
+
+async function authenticateUser(req, res) {
+  const user = await User.findByUserName(req.body.username);
+  if (!user) {
+      return res.status(401).json({ success: false, msg: 'Authentication failed. User not found.' });
+  }
+
+  const isMatch = await user.comparePassword(req.body.password);
+  if (isMatch) {
+      const token = jwt.sign({ username: user.username }, process.env.SECRET);
+      res.status(200).json({ success: true, token: 'BEARER ' + token });
+  } else {
+      res.status(401).json({ success: false, msg: 'Wrong password.' });
+  }
+}
 
 // Get all users
 router.get('/', async (req, res) => {
@@ -12,38 +35,23 @@ router.get('/', async (req, res) => {
 // register(Create)/Authenticate User
 const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
 
-router.post('/', async (req, res, next) => {
-    try {
-        if (req.query.action === 'register') {
-            if (!passwordRegex.test(req.body.password)) {
-              return res.status(400).json({
-                code: 400,
-                msg: 'Password must be at least 8 characters long and include at least 1 letter, 1 number, and 1 special character.',
-              });
-            }
-      
-            await User(req.body).save();
-            res.status(201).json({
-                code: 201,
-                msg: 'Successful created new user.',
-            });
+// register(Create)/Authenticate User
+router.post('/', asyncHandler(async (req, res) => {
+  try {
+      if (!req.body.username || !req.body.password) {
+          return res.status(400).json({ success: false, msg: 'Username and password are required.' });
       }
-      else { 
-          const user = await User.findOne(req.body);
-          if (!user) {
-              return res.status(401).json({ code: 401, msg: 'Authentication failed' });
-          } else {
-              return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' });
-          }
+      if (req.query.action === 'register') {
+          await registerUser(req, res);
+      } else {
+          await authenticateUser(req, res);
       }
-    } catch (err) {
-      if (err.name === 'ValidationError') {
-        const errors = Object.values(err.errors).map((el) => el.message);
-        return res.status(400).json({ code: 400, msg: `Validation error: ${errors.join(', ')}` });
-      }
-      next(err);
-    }
-  });
+  } catch (error) {
+      // Log the error and return a generic error message
+      console.error(error);
+      res.status(500).json({ success: false, msg: 'Internal server error.' });
+  }
+}));
 
 // Update a user
 router.put('/:id', async (req, res) => {
@@ -57,5 +65,7 @@ router.put('/:id', async (req, res) => {
       res.status(404).json({ code: 404, msg: 'Unable to Update User' });
   }
 });
+
+
 
 export default router;
